c99107424c871993d7f4ea86c8624e194eac4c66
Ricardo Markiewicz <ricardo.markiewicz@gmail.com>
2024-10-06 17:12:52 +0200

    Update examples

--- examples/dart_function/lib/dart_function.dart
+++ examples/dart_function/lib/dart_function.dart
@@ -4,7 +4,13 @@ import 'package:pocket_functions/pocket_functions.dart';
 Future<void> calculate(PocketRequest request) async {
   var a = int.tryParse(request.params["a"]?[0] ?? "") ?? 0;
   var b = int.tryParse(request.params["b"]?[0] ?? "") ?? 0;
-  var result = {"add": a + b};
+  var result = {
+    "add": a + b,
+    "minus:": a - b,
+    "mult": a * b,
+    "div": a / b,
+    "other": a ^ b
+  };
 
   request.response
       .addHeader("content-type", "application/json")
